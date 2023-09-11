0ff6376c71109f6f63c9cc784ae1f9ad9a07c773
Vladislav Balabaev <vv.balabaev@gmail.com>
2023-09-11 22:46:48 +0300

    add no directory error

--- convert_to_pdt.py
+++ convert_to_pdt.py
@@ -5,16 +5,24 @@ from pathlib import Path
 path = Path('to_convert')
 
 if __name__ == '__main__':
-    path.mkdir(parents=True, exist_ok=True)
-    
+    if not path.is_dir():
+        path.mkdir(parents=True, exist_ok=True)
+        raise FileNotFoundError('Please, add your images to newly created "to_convert" directory.')
+
     images = sorted(os.listdir(path))
     images = [Image.open(path / f) for f in images]
-    
+
     pdf_name = input("write name of the file> ")
     pdf_name = pdf_name.strip("'").strip('"').replace('.pdf', '')
+    pdf_name = f'{pdf_name}.pdf'
 
-    images[0].save(f'{pdf_name}.pdf',
+
+    images[0].save(pdf_name,
                    "PDF",
                    resolution=100.0, 
                    save_all=True, 
                    append_images=images[1:])
+
+    print(f'{pdf_name} has been created.')
+
+    exit()
